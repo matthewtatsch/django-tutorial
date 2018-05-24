9a09a3b1699747adda89023c0c293c8339f46e38
Matthew Tatsch <matt.tatsch@gmail.com>
2018-05-24 17:38:20 -0500

    DB settings for postgresql backend

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_tutorial',
+        'USER': 'admin',
+        'PASSWORD': 'AdminPW123$',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
