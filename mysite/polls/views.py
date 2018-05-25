5b2c63c829a4edf85341359b80e6bb9f019ffbad
Matthew Tatsch <matt.tatsch@gmail.com>
2018-05-25 11:15:55 -0500

    added index.html and detail.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,22 +2,27 @@
 polls views
 """
 
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.http import HttpResponse
 
 from .models import Question
 
 
 def index(request):
-    """polls index"""
+    """polls index
 
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_test for q in latest_question_list])
-    return HttpResponse(output)
+    Displays the five most recently published questions.
+    """
+
+    latest_question_list = get_list_or_404(Question.objects.order_by('-pub_date')[:5])
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     """detail for question"""
 
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     """results for question"""
